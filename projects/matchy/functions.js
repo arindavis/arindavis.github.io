f0aadd88ef0e74399780043ed3a3cbb356c3da65
Ubuntu <ubuntu@ip-172-31-91-85.ec2.internal>
2020-11-11 23:49:23 +0000

    A basic websitev4

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,32 +13,63 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 function search (array, animalName) {
-    if(array.includes(animalName)) {
         for(var i = 0; i < array.length; i++) {
-            if(array.name === animalName) {
+        //looping through our array, starting at 0 and ending at 
+            if(array[i]["name"].toUpperCase() === animalName.toUpperCase()) {
+            //if the name in our array object is strictly equal to our animal name
                 return array[i];
-            }
-        }
-    }
+            //return that name 
+        } 
+    } return null;
 }
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function replace (animals, name, replacement) {
+        for(var i = 0; i < animals.length; i++) {
+        //looping through our array, starting at 0 and ending at 
+            if(animals[i]["name"].toUpperCase() === name.toUpperCase()) {
+            //take away that indexed object and supplement our replacement
+            animals.splice(animals[i], 1, replacement);
+        } 
+    } 
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove (animals, name) {
+        for(var i = 0; i < animals.length; i++) {
+        //looping through our array, starting at 0 and ending at 
+            if(animals[i]["name"].toUpperCase() === name.toUpperCase()) {
+            //take away that indexed object 
+            animals.splice(animals[i], 1);
+        }
+    } 
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function add(animals, animal) {
+    //if our objects name, name length, species and species length are not equal to zero
+    if ((animal.name && animal.name.length !== 0) && (animal.species && animal.species.length !== 0)) {
+    //then iterate through our array
+        for (let i = 0; i < animals.length; i++) {
+        //and if this name matches with any of the other names in our array of objects
+            if (animals[i]['name'].toLowerCase() === animal['name'].toLowerCase()) {
+            //stop everything and return nothing
+                return;
+            }
+        }
+    //push animal onto our animals array
+        animals.push(animal);
+    }
+} 
 
 
 /**
